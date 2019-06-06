eb45230c8e9559ab6df2764fe840a52db2c2ae56
boli8 <boli8@iflytek.com>
2019-06-06 17:31:23 +0800

    add

--- development_skills.js
+++ development_skills.js
@@ -44,4 +44,187 @@ const num3 = 1.69 >> 0;
 // num1 num2 num3 => 1 1 1
 
 // 补零
+const FillZero = (num, len) => num.toString().padStart(len, "0");
+const num = FillZero(169, 5);
+// num => "00169"
+
+// 转数值：只对null、""、false、数值字符串有效
+const num1 = +null;
+const num2 = +"";
+const num3 = +false;
+const num4 = +"169";
+// num1 num2 num3 num4 => 0 0 0 169
+
+// 时间戳
+const timestamp = +new Date("2019-03-31");
+// timestamp => 1553990400000
+
+// 精确小数
+const RoundNum = (num, decimal) => Math.round(num * 10 ** decimal) / 10 ** decimal;
+const num = RoundNum(1.69, 1);
+// num => 1.7
+
+// 判断奇偶
+const OddEven = num => !!(num & 1) ? "odd" : "even";
+const num = OddEven(2);
+// num => "even"
+
+// 取最小最大值
+const arr = [0, 1, 2];
+const min = Math.min(...arr);
+const max = Math.max(...arr);
+// min max => 0 2
+
+// 生成范围随机数
+const RandomNum = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+const num = RandomNum(1, 10);
+
+/*-----------------------------------------------Boolean Skill----------------------------------------------------------------*/
+// 短路运算符
+const a = d && 1; // 满足条件赋值：取假运算，从左到右依次判断，遇到假值返回假值，后面不再执行，否则返回最后一个真值
+const b = d || 1; // 默认赋值：取真运算，从左到右依次判断，遇到真值返回真值，后面不再执行，否则返回最后一个假值
+const c = !d; // 取假赋值：单个表达式转换为true则返回false，否则返回true
+
+// 判断数据类型：undefined、null、string、number、boolean、array、object、symbol、date、regexp、function、asyncfunction、arguments、set、map、weakset、weakmap
+function DataType(tgt, type) {
+    const dataType = Object.prototype.toString.call(tgt).replace(/\[object /g, "").replace(/\]/g, "").toLowerCase();
+    return type ? dataType === type : dataType;
+}
+DataType("yajun"); // "string"
+DataType(19941112); // "number"
+DataType(true); // "boolean"
+DataType([], "array"); // true
+DataType({}, "array"); // false
+
+// 是否为空数组
+const arr = [];
+const flag = Array.isArray(arr) && !arr.length;
+// flag => true
+
+// 是否为空对象
+const obj = {};
+const flag = DataType(obj, "object") && !Object.keys(obj).length;
+// flag => true
+
+// 满足条件时执行
+const flagA = true; // 条件A
+const flagB = false; // 条件B
+(flagA || flagB) && Func(); // 满足A或B时执行
+(flagA || !flagB) && Func(); // 满足A或不满足B时执行
+flagA && flagB && Func(); // 同时满足A和B时执行
+flagA && !flagB && Func(); // 满足A且不满足B时执行
+
+// 为非假值时执行
+const flag = false; // undefined、null、""、0、false、NaN
+!flag && Func();
+
+// 数组不为空时执行
+const arr = [0, 1, 2];
+arr.length && Func();
+
+// 对象不为空时执行
+const obj = { a: 0, b: 1, c: 2 };
+Object.keys(obj).length && Func();
+
+// 函数退出代替条件分支退出
+if (flag) {
+    Func();
+    return false;
+}
+// 换成
+if (flag) {
+    return Func();
+}
+
+// switch/case使用区间
+const age = 26;
+switch (true) {
+    case isNaN(age):
+        console.log("not a number");
+        break;
+    case (age < 18):
+        console.log("under age");
+        break;
+    case (age >= 18):
+        console.log("adult");
+        break;
+    default:
+        console.log("please set your age");
+        break;
+}
+
+/*-----------------------------------------------Array Skill----------------------------------------------------------------*/
+// 克隆数组
+const _arr = [0, 1, 2];
+const arr = [..._arr];
+// arr => [0, 1, 2]
+
+// 合并数组
+const arr1 = [0, 1, 2];
+const arr2 = [3, 4, 5];
+const arr = [...arr1, ...arr2];
+// arr => [0, 1, 2, 3, 4, 5];
+
+// 去重数组
+const arr = [...new Set([0, 1, 1, null, null])];
+// arr => [0, 1, null]
+
+// 混淆数组
+const arr = [0, 1, 2, 3, 4, 5].slice().sort(() => Math.random() - .5);
+// arr => [3, 4, 0, 5, 1, 2]
+
+// 清空数组
+const arr = [0, 1, 2];
+arr.length = 0;
+// arr => []
+
+// 截断数组
+const arr = [0, 1, 2];
+arr.length = 2;
+// arr => [0, 1]
+
+// 交换赋值
+let a = 0;
+let b = 1;
+[a, b] = [b, a];
+// a b => 1 0
+
+// 过滤空值：undefined、null、""、0、false、NaN
+const arr = [undefined, null, "", 0, false, NaN, 1, 2].filter(Boolean);
+// arr => [1, 2]
+
+// 异步累计
+async function Func(deps) {
+    return deps.reduce(async(t, v) => {
+        const dep = await t;
+        const version = await Todo(v);
+        dep[v] = version;
+        return dep;
+    }, Promise.resolve({}));
+}
+const result = await Func(); // 需在async包围下使用
+
+// 数组首部插入成员
+let arr = [1, 2]; // 以下方法任选一种
+arr.unshift(0);
+arr = [0].concat(arr);
+arr = [0, ...arr];
+// arr => [0, 1, 2]
+
+//数组尾部插入成员
+let arr = [0, 1]; // 以下方法任选一种
+arr.push(2);
+arr.concat(2);
+arr[arr.length] = 2;
+arr = [...arr, 2];
+// arr => [0, 1, 2]
+
+// 统计数组成员个数
+const arr = [0, 1, 1, 2, 2, 2];
+const count = arr.reduce((t, c) => {
+    t[c] = t[c] ? ++ t[c] : 1;
+    return t;
+}, {});
+// count => { 0: 1, 1: 2, 2: 3 }
+
 
